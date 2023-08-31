2cb726221611a86f49dee2e0a0a163ef5af852da
Jefferson Reginaldo <141447493+Jefferson667@users.noreply.github.com>
2023-08-31 12:55:35 -0300

    test

---
+++ src/app/carro.tsx
@@ -0,0 +1,13 @@
+export default function Carro(){
+
+    return (
+        <div>
+
+            <button>
+
+                agenda igreja
+            
+            </button>
+        </div>
+    )
+}
