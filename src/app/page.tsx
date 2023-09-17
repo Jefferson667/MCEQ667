d618c6a2ce91148bc9087d883f283043cc19cc5a
Jefferson Reginaldo <141447493+Jefferson667@users.noreply.github.com>
2023-09-16 23:10:36 -0300

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,68 @@
 import Image from 'next/image'
-import Carro from './carro'
 
 export default function Home() {
-  return (
+  return (  
+    <main className='flex flex-col justify-center items-center'> 
+      <header className='flex flex-col justify-center items-center bg-gray-200 w-[800px] h-[300px] gap-5'>  
 
-  <div>
+      <Image
+        src="/logo.jpg"
+        width={100}
+        height={100}
+        alt="Picture of the author"
+      />
 
-    <Carro/>
-  </div>
+      <section>
+        <h1 className=' font-bold  text-[30px]'>
+          #MCEQ667
+        </h1>
+        <h1 className='flex justify-center'>
+          moto clube
+        </h1>
+      </section>
+
+      <nav>
+        <ul className='flex gap-5 cursor-pointer'>
+            <li><a>Entrar</a></li>
+            <li><a>dias</a></li>
+            <li><a>Agenda</a></li>
+            
+        </ul>
+      </nav>  
+      </header>
+    
+
+      <section className='flex justify-center mt-3'>
+        <iframe src="https://youtube.com/embed/6ZqQ23ngqEU?si=jgYQEs2x5Lul9ZnR" frameborder="0"></iframe>
+      </section>
+      
+        <h1 className='font-bold text-[25px] mt-8'>
+        Sobre Nós
+        </h1>
+
+        <h1 className='w-[800px] mt-3'>
+        "🏍️🙏 Unidos na Fé e na Estrada! 🙏🏍️ Motoclube da Igreja Quadrangular: Explorando a vida sobre duas rodas, guiados pela nossa fé. Juntos na jornada, juntos na devoção. 🕊️🛣️ #FéEstradaIgrejaQuadrangular"
+        </h1>
+
+        <h1 className='font-bold text-[25px] mt-24'> 
+        Contato
+        </h1>
+
+        <h1 className='mt-6'>
+        Entre em contato conosco pelo email: contato@motoclubeigreja.com
+        </h1>
+
+      <section>
+        
+      </section>
+
+    <footer className='flex justify-center bg-gray-200 w-[800px] h-[80px] items-center '>
+      <h1 className='flex'>
+      © 2023 Motoclube da Igreja. Todos os direitos reservados.
+      </h1>
+    </footer>
   
+    </main>
+
   )
 }
