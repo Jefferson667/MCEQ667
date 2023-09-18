b9e26804187128d15e0209bbf18b55619f89b4ad
Jefferson Reginaldo <141447493+Jefferson667@users.noreply.github.com>
2023-09-18 19:34:29 -0300

    Update page.tsx

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,9 +1,10 @@
-export default function Videos() {
+texport default function Videos() {
     return (  
       
-            <header className'text-[50px]'>
+            <header className'text-[100px]'>
                 <h1>
                     começo de algo novo....
+                    esquadrão quadrangula #mceq667
                 </h1>
             </header>
     )
