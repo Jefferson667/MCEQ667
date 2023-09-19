6c8404ab94c2593ae7c65a4dc8128203424193de
Jefferson Reginaldo <141447493+Jefferson667@users.noreply.github.com>
2023-09-19 00:19:45 -0300

    ok

--- src/app/fotos/page.tsx
+++ src/app/fotos/page.tsx
@@ -4,10 +4,17 @@ export default function Agenda() {
     return (  
      <main>
 
-        <section>
+        <section className='space-x-3'>
 
             <Image
                 src="/igreja.jpg"
+                width={800}
+                height={800}
+                alt="Picture of the author"
+            />
+
+            <Image
+                src="/qua.jgp"
                 width={800}
                 height={800}
                 alt="Picture of the author"
